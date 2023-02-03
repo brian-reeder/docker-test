2e9ef59f80c35e79df5efb539d1d7f7f31db457d
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2023-02-03 00:59:26 -0600

    Test failure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'docker-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('docker-test');
+    expect(app.title).toEqual('docker-test-bofa');
   });
 
   it('should render title', () => {
