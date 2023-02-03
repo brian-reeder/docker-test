fcd429455572920651e7094d5aac078975e10627
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2023-02-03 01:00:36 -0600

    Fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'docker-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('docker-test-bofa');
+    expect(app.title).toEqual('docker-test');
   });
 
   it('should render title', () => {
